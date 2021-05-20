61aadafa2f92f28f90ab4252ec9502a7984742c4
Erwin Pranata <erwin.pranata@admedika.co.id>
2021-05-20 11:21:08 +0700

    Updates

--- static/js/2.83afa7b7.chunk.js
+++
@@ -1,3 +0,0 @@
-/*! For license information please see 2.83afa7b7.chunk.js.LICENSE.txt */
-(this.webpackJsonpdashboard=this.webpackJsonpdashboard||[]).push([[2],{102:function(c,i,n){"use strict";n.d(i,"a",(function(){return r})),n.d(i,"b",(function(){return a}));var r={prefix:"fas",iconName:"arrow-right",icon:[448,512,[],"f061","M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"]},a={prefix:"fas",iconName:"circle",icon:[512,512,[],"f111","M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"]}}}]);
-//# sourceMappingURL=2.83afa7b7.chunk.js.map
